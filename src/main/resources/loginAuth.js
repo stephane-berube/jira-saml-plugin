9352b906d07b024bd21947c1b7c97e7d22a172cf
Stephane Berube <stephane.berube@canada.ca>
2019-04-25 09:10:23 -0400

    js: Ensure SSO form loads properly
    * Fix timing issues
    * Don't load SSO form on websudo screens

--- src/main/resources/loginAuth.js
+++ src/main/resources/loginAuth.js
@@ -1,87 +1,114 @@
-AJS.$(function() {
-    setTimeout(function() {
-        if (AJS.$("#login-form").length) {
-            loadCorpLogin(AJS.$("#login-form"));
-        } else if (AJS.$("#loginform").length) {
-            loadCorpLogin(AJS.$("#loginform"));
-        } else {
-            AJS.$("iframe").ready(function() {
+/*global AJS: false*/
+( function( $ ) {
+    var formIsLoaded = false,
 
-                var iframe = AJS.$("#gadget-0")
-                iframe.load(function() {
-                    loginForm = AJS.$("#" + iframe[0].id).contents().find("#loginform")
-                    loadCorpLogin(loginForm);
-                });
-            });
-        } // Your code here
-    }, 0);
+        loadCorpLogin = function( $loginForm ) {
+            var query = location.search.substr( 1 ),
 
-    function loadCorpLogin(loginForm) {
-        if (loginForm.length == 1) {
-            loginFormId = loginForm[0].id
-            loginForm.hide();
+                redirectMessage = "Please wait while we redirect you to your company log in page",
 
-            if (loginFormId == "login-form" || loginFormId == "loginform") {
-                AJS.$('<div class="field-group"><a class="aui-button aui-style aui-button-primary" href="' + AJS.contextPath() + '/plugins/servlet/saml/auth" style="align:center;">Use Corporate login</a></div><h2 style="margin-top:10px"></h2>').insertBefore(AJS.$("#" + loginFormId + " .field-group:first-child"));
-            } else {
-                AJS.$('<div class="field-group"><a class="aui-button aui-style aui-button-primary" href="' + AJS.contextPath() + '/plugins/servlet/saml/auth" style="margin-left:100px;margin-top:5px;">Use Corporate login</a></div>').insertBefore(AJS.$("#gadget-0"));
-            }
+                hideNativeLoginForm = true,
 
-            // keep track of whether we allow Forced SSO Login, regardless of
-            // user prefs.  (this is needed to prevent loops if SSO login fails)
-            var forcedSsoLoginAllowed = true;
-            
-            var query = location.search.substr(1);
-            query.split("&").forEach(function(part) {
-                var item = part.split("=");
-                if (item.length == 2 && item[0] == "samlerror") {
-                    var errorKeys = {};
-                    errorKeys["general"] = "General SAML configuration error";
-                    errorKeys["user_not_found"] = "User was not found";
-                    errorKeys["plugin_exception"] = "SAML plugin internal error";
-                    loginForm.show();
-                    var message = '<div class="aui-message closeable error">' + errorKeys[item[1]] + '</div>';
-                    AJS.$(message).insertBefore(loginForm);
-                    
-                    // there's a SAML error, so don't keep trying to login
-                    forcedSsoLoginAllowed = false;
-                    console.log("disabling forced login");
-                }
-            });
+                // keep track of whether we allow Forced SSO Login, regardless of
+                // user prefs.  (this is needed to prevent loops if SSO login fails)
+                forcedSsoLoginAllowed = true;
 
-            if (location.search == '?logout=true') {
-                $.ajax({
-                    url: AJS.contextPath() + "/plugins/servlet/saml/getajaxconfig?param=logoutUrl",
-                    type: "GET",
-                    error: function() {},
-                    success: function(response) {
-                        if (response != "") {
-                            AJS.$('<p>Please wait while we redirect you to your company log out page</p>').insertBefore(loginForm);
-                            window.location.href = response;
-                            return;
-                        }
-                    }
-                });
-                return;
-            }
-
-            AJS.$.ajax({
+            // TODO:
+            //  Keycloak-initiated login:
+            //    - If we're not logged-in to JIRA, check if user is logged in (via custom iframe or keycloak js adapter)
+            //    - If user is logged in SSO but not JIRA, redirect to SSO so that JIRA knows we're logged in
+            //
+            //  Keycloak-initiated logout (maybe?):
+            //    - If we're logged-in to JIRA and we're a SSO user, check (at intervals) if user is still logged-in to SSO
+            //    - If not, logout of JIRA (or have a "Session timed-out, relogin _here_" popup message thing
+            //
+            //  JIRA-initiated logout:
+            //    - If we're an SSO user, logout of Keycloak, then logout of JIRA
+            $.ajax( {
                 url: AJS.contextPath() + "/plugins/servlet/saml/getajaxconfig?param=idpRequired",
                 type: "GET",
                 error: function() {},
-                success: function(response) {
-                    if ( response == "true" && forcedSsoLoginAllowed ) {
-                        // AJS.$('<img src="download/resources/com.bitium.confluence.SAML2Plugin/images/progress.png"/>').insertBefore(AJS.$(".aui.login-form-container"));
-                        AJS.$('<p>Please wait while we redirect you to your company log in page</p>').insertBefore(loginForm);
-                        window.location.href = AJS.contextPath() + '/plugins/servlet/saml/auth';
+                success: function( response ) {
+                    if ( response === "true" && forcedSsoLoginAllowed ) {
+                        $( "<p>" + redirectMessage + "</p>" ).insertBefore( $loginForm );
 
-                    } else {
-                        loginForm.show();
+                        window.location.href = AJS.contextPath() + "/plugins/servlet/saml/auth";
                     }
                 }
-            });
+            } );
 
+            if ( formIsLoaded === true ) {
+                return;
+            }
+
+            if ( location.search === "?logout=true" ) {
+                $.ajax( {
+                    url: AJS.contextPath() + "/plugins/servlet/saml/getajaxconfig?param=logoutUrl",
+                    type: "GET",
+                    error: function() {},
+                    success: function( response ) {
+                        if ( response !== "" ) {
+                            $( "<p>" + redirectMessage + "</p>" ).insertBefore( $loginForm );
+
+                            window.location.href = response;
+                        }
+                    }
+                } );
+
+                return;
+            }
+
+            query.split( "&" ).forEach( function( part ) {
+                var item = part.split( "=" );
+
+                if ( item.length === 2 && item[ 0 ] === "nosaml" ) {
+                    hideNativeLoginForm = false;
+                }
+
+                if ( item.length === 2 && item[ 0 ] === "samlerror" ) {
+                    var errorKeys = {},
+                        message = "";
+
+                    errorKeys[ "general" ] = "General SAML configuration error";
+                    errorKeys[ "user_not_found" ] = "User was not found";
+                    errorKeys[ "plugin_exception" ] = "SAML plugin internal error";
+
+                    $loginForm.show();
+
+                    message = "<div class='aui-message closeable error'>" + errorKeys[ item[ 1 ] ] + "</div>";
+                    $( message ).insertBefore( $loginForm );
+
+                    // there's a SAML error, so don't keep trying to login
+                    forcedSsoLoginAllowed = false;
+                }
+            } );
+
+            if ( hideNativeLoginForm === true ) {
+                $loginForm.hide();
+                $( "<div class='field-group' style='text-align: center;'><a class='aui-button aui-style aui-button-primary' href='" + AJS.contextPath() + "/plugins/servlet/saml/auth' style='align:center;'>Use Corporate login</a></div>" ).insertBefore( $loginForm );
+                formIsLoaded = true;
+            }
+        };
+
+    // Login form on the /login.jsp page
+    $( document ).ready( function() {
+        var $loginForm = $( "#login-form" ),
+            isWebSudo = ( $loginForm.attr( "action" ) === "/secure/admin/WebSudoAuthenticate.jspa" );
+
+        if ( $loginForm.length === 1 && !isWebSudo ) {
+            loadCorpLogin( $loginForm );
         }
-    }
+    } );
 
-});
+    // Login form on the dashboard
+    //
+    // document.ready (sometimes) doesn't work here since
+    // the dashboard is built with JS
+    $( document ).on( "gadgets-loaded", function( e ) {
+        var $loginForm = $( "#loginform" );
+
+        if ( $loginForm.length === 1 ) {
+            loadCorpLogin( $loginForm );
+        }
+    } );
+}( AJS.$ ) );
